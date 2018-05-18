7e4e9a334b951aabf290a298170be0b6d6c8b483
Manon18 <maryland16z18@gmail.com>
2018-05-18 17:55:56 +0300

    difference between event.stopPropagation and event.preventDefault

---
+++ task_5.stop_and_prevent_events/script.js
@@ -0,0 +1,32 @@
+var first_box1 = document.getElementsByClassName("first_box1")[0];
+var first_box2 = document.getElementsByClassName("first_box2")[0];
+var first_box3 = document.getElementsByClassName("first_box3")[0];
+var button = document.getElementsByClassName("button")[0];
+
+
+var onClickOne = function (e) {
+  var $elOne = e.target;
+  
+  e.stopPropagation();
+  
+  alert('CLICK!');
+  var timer = setTimeout(function(){
+    $elOne.classList.toggle("box_one");
+  }, 100);
+}
+
+document.body.onclick = onClickOne;
+first_box1.onclick = onClickOne;
+first_box2.onclick = onClickOne;
+first_box3.onclick = onClickOne;
+
+var onClickTwo = function (el) {
+  var $elTwo = el.target;
+  
+  el.preventDefault();
+  
+  alert("You Don't Have Permission to Access! Sorry =_(");
+}
+
+document.body.onclick = onClickTwo;
+button.onclick = onClickTwo;
